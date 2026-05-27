a8c7b03a9dbf915c5db3d3a81444e2eddea15520
Vladyslav Korzun <256404775+Vladyslav-Korzun@users.noreply.github.com>
2026-05-27 18:10:58 +0200

    fix(env,navbar): migrate domain to hotel-reservation-system.com, fix tablet breakpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: '/api',
   auth: {
-    issuer: 'https://korzunv.98.71.77.25.nip.io/auth/realms/hotel-reservation',
+    issuer: 'https://hotel-reservation-system.com/auth/realms/hotel-reservation',
     clientId: 'hotel-reservation-frontend',
     requireHttps: true,
   },
